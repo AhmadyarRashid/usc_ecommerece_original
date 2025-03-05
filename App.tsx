82e5d1e271497479e00ba99fb667f81c13dd264c
saranahmed <saranahmed192@gmail.com>
2025-03-05 14:56:38 +0500

    update react native version to 0.78

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
-import { SafeAreaView } from "react-native";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import NetworkLogger from "react-native-network-logger";
-import Toast from "react-native-toast-message";
-import { I18nextProvider } from "react-i18next";
+import {SafeAreaView} from 'react-native';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import NetworkLogger from 'react-native-network-logger';
+import Toast from 'react-native-toast-message';
+import {I18nextProvider} from 'react-i18next';
 
-import AppNavigation from "./src/navigation";
+import AppNavigation from './src/navigation';
 
-import { persistor, store } from "./src/redux/store";
-import i18n from "./src/localization/i18n";
+import {persistor, store} from './src/redux/store';
+import i18n from './src/localization/i18n';
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{flex: 1}}>
       <BottomSheetModalProvider>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{flex: 1}}>
           <Provider store={store}>
             <PersistGate
               // loading={<Text>Loading...</Text>}
-              persistor={persistor}
-            >
+              persistor={persistor}>
               <I18nextProvider i18n={i18n}>
                 <AppNavigation />
               </I18nextProvider>
